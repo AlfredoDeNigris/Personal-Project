95c81216de99270b93622d583f21f46a92aaca49
AlfredoDeNigris <124916313+AlfredoDeNigris@users.noreply.github.com>
2024-07-20 03:22:10 -0300

    Add files via upload

--- database/database.sql
+++ database/database.sql
@@ -6,59 +6,61 @@ USE personal_project;
 CREATE TABLE IF NOT EXISTS client(
   client_id INT PRIMARY KEY AUTO_INCREMENT,
   full_name VARCHAR(100) NOT NULL,
-  username VARCHAR(100) NOT NULL,
+  username VARCHAR(100) UNIQUE NOT NULL,
   password VARCHAR(100) NOT NULL,
-  billing_address VARCHAR(255) UNIQUE NOT NULL,
+  billing_address VARCHAR(100) NOT NULL,
   phone_number VARCHAR(15) UNIQUE NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS budget(
-  budget_id INT PRIMARY KEY AUTO_INCREMENT,
-  client_id INT NOT NULL,
-  date DATE NOT NULL,
-  amount DECIMAL(10,2) NOT NULL,
-  FOREIGN KEY (client_id) REFERENCES client (client_id)
-);
-
-CREATE TABLE IF NOT EXISTS house(
-  house_id INT PRIMARY KEY AUTO_INCREMENT,
-  review TEXT,
+CREATE TABLE IF NOT EXISTS house_model(
+  house_model_id INT PRIMARY KEY AUTO_INCREMENT,
+  review TEXT NOT NULL,
   construction_time INT NOT NULL,
-  bathrooms INT NOT NULL,
-  bedrooms INT NOT NULL,
+  bathroom INT NOT NULL,
+  bedroom INT NOT NULL,
   square_meters INT NOT NULL,
-  features_cost DECIMAL(10,2) NOT NULL,
   worker_cost DECIMAL(10,2) NOT NULL,
   comercial_cost DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS picture(
-  pitcure_id INT PRIMARY KEY AUTO_INCREMENT,
-  house_id INT NOT NULL,
-  FOREIGN KEY (house_id) REFERENCES house (house_id)
+CREATE TABLE IF NOT EXISTS house_picture(
+  house_pitcure_id INT PRIMARY KEY AUTO_INCREMENT,
+  house_model_id INT NOT NULL,
+  picture LONGBLOB NOT NULL,
+  FOREIGN KEY (house_model_id) REFERENCES house_model (house_model_id)
 );
 
-CREATE TABLE IF NOT EXISTS available_house(
-  budget_id INT NOT NULL,
-  house_id INT NOT NULL,
-  PRIMARY KEY (budget_id, house_id),
-  FOREIGN KEY (budget_id) REFERENCES budget (budget_id),
-  FOREIGN KEY (house_id) REFERENCES house (house_id)
+CREATE TABLE IF NOT EXISTS selected_house(
+  client_id INT NOT NULL,
+  house_model_id INT NOT NULL,
+  final_price DECIMAL (10,2) NOT NULL,
+  PRIMARY KEY (client_id, house_model_id),
+  FOREIGN KEY (client_id) REFERENCES client (client_id),
+  FOREIGN KEY (house_model_id) REFERENCES house_model (house_model_id)
 );
 
 CREATE TABLE IF NOT EXISTS feature(
   feature_id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
+  feature_name VARCHAR(100) NOT NULL,
   unit_cost DECIMAL(10,2) NOT NULL,
-  information VARCHAR(45) NOT NULL
+  information VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS house_feature(
-  house_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS feature_picture(
+  feature_pitcure_id INT PRIMARY KEY AUTO_INCREMENT,
   feature_id INT NOT NULL,
-  PRIMARY KEY (house_id, feature_id),
+  picture LONGBLOB NOT NULL,
+  FOREIGN KEY (feature_id) REFERENCES feature (feature_id)
+);
+
+CREATE TABLE IF NOT EXISTS selected_house_feature(
+  client_id INT NOT NULL,
+  house_model_id INT NOT NULL,
+  feature_id INT NOT NULL,
+  PRIMARY KEY (client_id, house_model_id, feature_id),
   quantity INT NOT NULL,
-  FOREIGN KEY (house_id) REFERENCES house (house_id),
+  FOREIGN KEY (client_id) REFERENCES client (client_id),
+  FOREIGN KEY (house_model_id) REFERENCES house_model (house_model_id),
   FOREIGN KEY (feature_id) REFERENCES feature (feature_id)
 );
