5757895fa9456cbc69fc804df25f6765d22f88df
AlfredoDeNigris <ignaciodenigris@gmail.com>
2024-07-26 21:17:01 -0300

    dockerized project

--- index.js
+++ index.js
@@ -15,40 +15,54 @@ const sqlSetup = fs.readFileSync('./database/database.sql', 'utf8');
 const defaultOptions = fs.readFileSync('./database/defaultOptions.sql', 'utf8');
 
 //Create a connection to the database
-const connection = mysql.createConnection({
-    host: config.database.host,
-    user: config.database.user,
-    password: config.database.password,
-    database: config.database.name,
-    multipleStatements: true // This allows multiple statements in a single query
-});
-
-//Connect to the database and execute the SQL scripts
-connection.connect(err => {
-    if (err) {
-        console.error('Error connecting to the database:', err.message);
-        return;
-    }
-    console.log('Connected to the database.');
-
-    //Execute setup script
-    connection.query(sqlSetup, (err, results) => {
-        if (err) {
-            console.error('Error executing the setup SQL script:', err.message);
-        } else {
-            console.log('Setup SQL script executed successfully.');
-
-            //Execute default options script
-            connection.query(defaultOptions, (err, results) => {
-                if (err) {
-                    console.error('Error executing the default options SQL script:', err.message);
-                } else {
-                    console.log('Default options SQL script executed successfully.');
-                }
-            });
-        }
+const createConnection = () => {
+    return mysql.createConnection({
+        host: process.env.MYSQL_HOST || 'db',
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASSWORD || '',
+        database: process.env.MYSQL_DATABASE || 'personal_project',
+        multipleStatements: true,
+        port: process.env.MYSQL_PORT || 3306
     });
+};
+
+
+setTimeout(() => {
+    connection = createConnection();
+
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to the database:', err.message);
+            return;
+        }
+        console.log('Connected to the database.');
+
+        //Execute setup script
+        connection.query(sqlSetup, (err, results) => {
+            if (err) {
+                console.error('Error executing the setup SQL script:', err.message);
+            } else {
+                console.log('Setup SQL script executed successfully.');
+
+                //Execute default options script
+                connection.query(defaultOptions, (err, results) => {
+                    if (err) {
+                        console.error('Error executing the default options SQL script:', err.message);
+                    } else {
+                        console.log('Default options SQL script executed successfully.');
+                    }
+                });
+            }
+        });
+    });
+}, 2000);
+
+
+//Default route to check server's status
+app.get('/isAlive', (req, res) => {
+    res.sendStatus(200);
 });
+
 
 //Middleware to make the connection available to routes
 app.use((req, res, next) => {
@@ -68,7 +82,7 @@ app.listen(config.server.port, (err) => {
 
 //Handle server shutdown
 const shutdownConnection = () => {
-    console.log('Shutting down database connection ');
+    console.log('Shutting down database connection');
     connection.end(err => {
         if (err) {
             console.error('Error closing the connection:', err.message);
